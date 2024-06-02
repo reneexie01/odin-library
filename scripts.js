5c2b9bae3f95a6bf946934e2f3a94972934ae137
Renee <156127959+reneexie01@users.noreply.github.com>
2024-06-02 15:09:22 +1000

    Added basic form questions

---
+++ scripts.js
@@ -0,0 +1,11 @@
+const myLibrary = [];
+
+/* Constructor function */
+function Book(title, author, pages, readStatus) {
+    return `${title} by ${author}, ${pages} pages, ${readStatus}`;
+};
+
+/* Input function */
+function addBookToLibrary() {
+
+}
