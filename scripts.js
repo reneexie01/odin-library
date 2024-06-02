56222b678e919d6f612a21283260b2a1277c1c63
Renee <156127959+reneexie01@users.noreply.github.com>
2024-06-03 06:03:34 +1000

    DOM setup to show myLibrary and add new submissions

--- scripts.js
+++ scripts.js
@@ -1,4 +1,8 @@
-const myLibrary = [];
+const myLibrary = [
+    {title: 'Brave New World1', author: 'Aldous Huxley1', pages: '3111', readStatus: 'read1'},
+    {title: 'Brave New World2', author: 'Aldous Huxley2', pages: '3112', readStatus: 'read2'},
+    {title: 'Brave New World3', author: 'Aldous Huxley3', pages: '3113', readStatus: 'read3'},
+];
 
 /* Constructor function */
 function Book(title, author, pages, readStatus) {
@@ -14,6 +18,7 @@ function addBookToLibrary(title, author, pages, readStatus) {
     myLibrary.push(newBook);
 }
 
+/* Adding new submissions */
 const submitButton = document.querySelector('.submitButton');
 
 submitButton.addEventListener('click', () => {
@@ -23,5 +28,25 @@ submitButton.addEventListener('click', () => {
     const readStatusValue = document.querySelector('#readStatus').value;
 
     addBookToLibrary(titleValue, authorValue, pagesValue, readStatusValue);
+    const lastBook = myLibrary[myLibrary.length - 1];
+    printBooks(lastBook);
     console.log(myLibrary);
 })
+
+/* Printing the library */
+myLibrary.forEach((book) => printBooks(book));
+
+function printBooks(book) {
+    const libraryContainer = document.querySelector('.library-container');
+    const libraryBook = document.createElement('div');
+    libraryBook.classList.add('library-book');
+
+    libraryBook.textContent = 
+    `
+    Title: ${book.title}
+    Author: ${book.author}
+    Pages: ${book.pages}
+    Read Status: ${book.readStatus}
+    `;
+    libraryContainer.appendChild(libraryBook);
+}
