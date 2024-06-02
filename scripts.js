774f88413077c7feef2f5c8ca9d514d51b1bcee9
Renee <156127959+reneexie01@users.noreply.github.com>
2024-06-02 15:43:08 +1000

    Input function to add books to myLibrary array

--- scripts.js
+++ scripts.js
@@ -2,10 +2,26 @@ const myLibrary = [];
 
 /* Constructor function */
 function Book(title, author, pages, readStatus) {
-    return `${title} by ${author}, ${pages} pages, ${readStatus}`;
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.readStatus = readStatus;
 };
 
 /* Input function */
-function addBookToLibrary() {
-
+function addBookToLibrary(title, author, pages, readStatus) {
+    const newBook = new Book(title, author, pages, readStatus)
+    myLibrary.push(newBook);
 }
+
+const submitButton = document.querySelector('.submitButton');
+
+submitButton.addEventListener('click', () => {
+    const titleValue = document.querySelector('#title').value;
+    const authorValue = document.querySelector('#author').value;
+    const pagesValue = document.querySelector('#pages').value;
+    const readStatusValue = document.querySelector('#readStatus').value;
+
+    addBookToLibrary(titleValue, authorValue, pagesValue, readStatusValue);
+    console.log(myLibrary);
+})
