f7ae70694b5a3d036fd0a34efe7d913e074c73c5
Renee <156127959+reneexie01@users.noreply.github.com>
2024-09-05 16:19:39 +1000

    Used prettier and removed some unnecessary console logs

--- scripts.js
+++ scripts.js
@@ -1,24 +1,34 @@
 let myLibrary = [
-    {title: "Brave New World", author: "Aldous Huxley", pages: "311", readStatus: "read"},
-    {title: "1984", author: "George Orwell", pages: "384", readStatus: "read"},
-    {title: "Lord of the Flies", author: "William Golding", pages: "224", readStatus: "unread"},
+  {
+    title: "Brave New World",
+    author: "Aldous Huxley",
+    pages: "311",
+    readStatus: "read",
+  },
+  { title: "1984", author: "George Orwell", pages: "384", readStatus: "read" },
+  {
+    title: "Lord of the Flies",
+    author: "William Golding",
+    pages: "224",
+    readStatus: "unread",
+  },
 ];
 
 /* Class constructor version */
 class Book {
-    constructor(title, author, pages, readStatus) {
-        this.title = title;
-        this.author = author;
-        this.pages = pages;
-        this.readStatus = readStatus;    
-    }
+  constructor(title, author, pages, readStatus) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.readStatus = readStatus;
+  }
 
-    toggleReadStatus() {
-        this.readStatus = this.readStatus === "read" ? "unread" : "read";
-    }
+  toggleReadStatus() {
+    this.readStatus = this.readStatus === "read" ? "unread" : "read";
+  }
 
-    displayBook() {
-        return `
+  displayBook() {
+    return `
         <ul>
             <li>Title: ${this.title}</li>
             <li>Author: ${this.author}</li>
@@ -28,62 +38,66 @@ class Book {
         <button class="update-book">Update</button>
         <button class="delete-book">Delete</button>
         `;
-    }
+  }
 
-    static validateBook(book) {
-        return book.title && book.author && book.pages && book.readStatus;
-    }
+  static validateBook(book) {
+    return book.title && book.author && book.pages && book.readStatus;
+  }
 }
 
 /* Mapping existing books to class */
-myLibrary = myLibrary.map((book) => new Book(book.title, book.author, book.pages, book.readStatus));
+myLibrary = myLibrary.map(
+  (book) => new Book(book.title, book.author, book.pages, book.readStatus)
+);
 
 /* Input function */
 function addBookToLibrary(title, author, pages, readStatus) {
-    const newBook = new Book(title, author, pages, readStatus)
-    myLibrary.push(newBook);
+  const newBook = new Book(title, author, pages, readStatus);
+  myLibrary.push(newBook);
 }
 
 /* Adding new submissions */
 const submitButton = document.querySelector(".submitButton");
 
 submitButton.addEventListener("click", () => {
-    const titleValue = document.querySelector("#title").value;
-    const authorValue = document.querySelector("#author").value;
-    const pagesValue = document.querySelector("#pages").value;
-    const readStatusValue = document.querySelector("#readStatus").value;
+  const titleValue = document.querySelector("#title").value;
+  const authorValue = document.querySelector("#author").value;
+  const pagesValue = document.querySelector("#pages").value;
+  const readStatusValue = document.querySelector("#readStatus").value;
 
-    const badSubmission = document.querySelector(".bad-submission");
+  const badSubmission = document.querySelector(".bad-submission");
 
-    const newBook = new Book(titleValue, authorValue, pagesValue, readStatusValue);
+  const newBook = new Book(
+    titleValue,
+    authorValue,
+    pagesValue,
+    readStatusValue
+  );
 
-    if (!Book.validateBook(newBook)) {
-        badSubmission.textContent = `Invalid submission. Try again.`
-        return;
-    } 
-    
-    addBookToLibrary(titleValue, authorValue, pagesValue, readStatusValue);
-    updateLibrary();
-    console.log(myLibrary);
-})
+  if (!Book.validateBook(newBook)) {
+    badSubmission.textContent = `Invalid submission. Try again.`;
+    return;
+  }
+
+  addBookToLibrary(titleValue, authorValue, pagesValue, readStatusValue);
+  updateLibrary();
+});
 
 /* Printing the library */
 myLibrary.forEach((book, index) => printBooks(book, index));
 
 function printBooks(book, index) {
-    const libraryContainer = document.querySelector(".library-container");
-    const libraryBook = document.createElement("div");
+  const libraryContainer = document.querySelector(".library-container");
+  const libraryBook = document.createElement("div");
 
-    libraryBook.innerHTML = book.displayBook();
-    libraryContainer.appendChild(libraryBook);
+  libraryBook.innerHTML = book.displayBook();
+  libraryContainer.appendChild(libraryBook);
 
-    const deleteButton = libraryBook.querySelector(".delete-book");
-    deleteButton.addEventListener("click", () => deleteBook(index));
-        
-    const updateButton = libraryBook.querySelector(".update-book");
-    updateButton.addEventListener("click", () => updateBook(index));
+  const deleteButton = libraryBook.querySelector(".delete-book");
+  deleteButton.addEventListener("click", () => deleteBook(index));
 
-    console.log(myLibrary);
+  const updateButton = libraryBook.querySelector(".update-book");
+  updateButton.addEventListener("click", () => updateBook(index));
 }
 
 /* Modal form */
@@ -92,31 +106,31 @@ const showButton = document.querySelector(".open-modal");
 const closeButton = document.querySelector(".close-modal");
 
 showButton.addEventListener("click", () => {
-    dialog.showModal();
+  dialog.showModal();
 });
 
 closeButton.addEventListener("click", () => {
-    dialog.close();
+  dialog.close();
 });
 
 submitButton.addEventListener("click", () => {
-    dialog.close();
-})
+  dialog.close();
+});
 
 /* Delete functions */
 function deleteBook(index) {
-    myLibrary.splice(index, 1);
-    updateLibrary();
+  myLibrary.splice(index, 1);
+  updateLibrary();
 }
 
 function updateLibrary() {
-    const libraryContainer = document.querySelector(".library-container");
-    libraryContainer.innerHTML = "";
-    myLibrary.forEach((book, index) => printBooks(book, index));
+  const libraryContainer = document.querySelector(".library-container");
+  libraryContainer.innerHTML = "";
+  myLibrary.forEach((book, index) => printBooks(book, index));
 }
 
 /* Update functions */
 function updateBook(index) {
-    myLibrary[index].toggleReadStatus();
-    updateLibrary();
+  myLibrary[index].toggleReadStatus();
+  updateLibrary();
 }
